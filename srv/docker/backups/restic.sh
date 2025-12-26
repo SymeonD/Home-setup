fd182d2796d2d30c1933e114dc8ad63eaf10788c
symeon <symeon.dealmeida@gmail.com>
2025-12-26 22:11:07 +0000

    Finish backup and restore scripts

--- srv/docker/backups/restic.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-export RESTIC_REPOSITORY=/mnt/backups/restic
-export RESTIC_PASSWORD=supersecret
-
-
-restic backup /srv/docker
-restic forget --keep-daily 7 --keep-weekly 4 --keep-monthly 2 --prune
