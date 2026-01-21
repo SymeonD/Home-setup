a511e4df100b832813c023029031c0356d0bb15a
Symeon De Almeida <symeon.dealmeida@gmail.com>
2026-01-21 20:24:00 +0100

    Add new ansible setup and remove old one

---
+++ Home-Setup/ansible/roles/backup/files/backup_restic.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+export RESTIC_REPOSITORY=/srv/backup/restic-repo
+export RESTIC_PASSWORD="{{ restic_password }}"
+
+# Initialiser le repo si nécessaire
+if [ ! -f "$RESTIC_REPO/config" ]; then
+    restic init --repo $RESTIC_REPO
+fi
+
+# Immich uploads
+nice -n 19 ionice -c2 -n7 restic backup --limit-upload 5 --limit-download 5 /srv/data/immich/ -v --tag immich
+
+# Nextcloud data
+nice -n 19 ionice -c2 -n7 restic backup --limit-upload 5 --limit-download 5 /srv/data/nextcloud/ -v --tag nextcloud
+
+# Supprimer les snapshots vieux de plus de 30 jours
+restic forget --keep-daily 7 --keep-weekly 4 --keep-monthly 12 --prune
