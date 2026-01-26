1324df8d6d18b6c17f5e0d103a4eb932aa3d3dc3
Symeon De Almeida <symeon.dealmeida@gmail.com>
2026-01-26 12:05:21 +0100

    change restic to rstic-repo

--- srv/backup-scripts/backup_postgres.sh
+++ srv/backup-scripts/backup_postgres.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-BACKUP_DIR="/mnt/backups/postgres/$(date +%F)"
+BACKUP_DIR="/srv/backup/postgres/$(date +%F)"
 mkdir -p "$BACKUP_DIR"
 
 docker exec immich-postgres pg_dump -Fc -U immich immich > "$BACKUP_DIR/immich.dump"
 docker exec nextcloud-postgres pg_dump -Fc -U nc nextcloud > "$BACKUP_DIR/nextcloud.dump"
 
 # Nettoyage ancien backup
-find /mnt/backups/postgres/ -maxdepth 1 -type d -mtime +30 -exec rm -rf {} \;
+find /srv/backup/postgres/ -maxdepth 1 -type d -mtime +30 -exec rm -rf {} \;
