34e171c544d43892db2acff96f4a60e6c73c7ff3
Symeon De Almeida <symeon.dealmeida@gmail.com>
2026-01-27 18:23:59 +0100

    Fix restic script taking too long

--- Home-Setup/ansible/roles/backup/templates/backup_restic.sh
+++ Home-Setup/ansible/roles/backup/templates/backup_restic.sh
@@ -9,10 +9,10 @@ if [ ! -f "$RESTIC_REPO/config" ]; then
 fi
 
 # Immich uploads
-nice -n 19 ionice -c2 -n7 restic backup --repo $RESTIC_REPO --limit-upload 5 --limit-download 5 /srv/data/immich/ -v --tag immich
+nice -n 19 ionice -c2 -n7 restic backup --repo $RESTIC_REPO /srv/data/immich/ -v --tag immich
 
 # Nextcloud data
-nice -n 19 ionice -c2 -n7 restic backup --repo $RESTIC_REPO --limit-upload 5 --limit-download 5 /srv/data/nextcloud/ -v --tag nextcloud
+nice -n 19 ionice -c2 -n7 restic backup --repo $RESTIC_REPO /srv/data/nextcloud/ -v --tag nextcloud
 
 # Supprimer les snapshots vieux de plus de 30 jours
 restic forget --keep-daily 7 --keep-weekly 4 --keep-monthly 12 --prune
