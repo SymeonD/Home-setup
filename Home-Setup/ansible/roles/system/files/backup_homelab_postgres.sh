14bad02cbed1379b9bb173bbf68a1d4e4c53fe64
Symeon De Almeida <symeon.dealmeida@gmail.com>
2026-01-28 22:01:18 +0100

    fix immich-postgres container name

--- Home-Setup/ansible/roles/system/files/backup_homelab_postgres.sh
+++ Home-Setup/ansible/roles/system/files/backup_homelab_postgres.sh
@@ -3,7 +3,7 @@
 BACKUP_DIR="/srv/backup/postgres/$(date +%F)"
 mkdir -p "$BACKUP_DIR"
 
-docker exec -t immich_postgres pg_dumpall --clean --if-exists --username=immich | gzip > "$BACKUP_DIR/immich_dump.sql.gz"
+docker exec -t immich-postgres pg_dumpall --clean --if-exists --username=immich | gzip > "$BACKUP_DIR/immich_dump.sql.gz"
 
 # docker exec immich-postgres pg_dump -Fc -U immich immich > "$BACKUP_DIR/immich.dump"
 docker exec nextcloud-postgres pg_dump -Fc -U nc nextcloud > "$BACKUP_DIR/nextcloud.dump"
