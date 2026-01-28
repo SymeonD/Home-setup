8e4b6620221f0d93adb0d77a0531f08951849e9b
Symeon De Almeida <symeon.dealmeida@gmail.com>
2026-01-28 22:09:53 +0100

    Update postgres backup system

--- Home-Setup/ansible/roles/system/files/backup_homelab_postgres.sh
+++ Home-Setup/ansible/roles/system/files/backup_homelab_postgres.sh
@@ -3,10 +3,11 @@
 BACKUP_DIR="/srv/backup/postgres/$(date +%F)"
 mkdir -p "$BACKUP_DIR"
 
+# Immich
 docker exec -t immich-postgres pg_dumpall --clean --if-exists --username=immich | gzip > "$BACKUP_DIR/immich_dump.sql.gz"
 
-# docker exec immich-postgres pg_dump -Fc -U immich immich > "$BACKUP_DIR/immich.dump"
-docker exec nextcloud-postgres pg_dump -Fc -U nc nextcloud > "$BACKUP_DIR/nextcloud.dump"
+# Nextcloud
+docker exec -t nextcloud-postgres pg_dumpall --clean --if-exists --username=nc | gzip > "$BACKUP_DIR/nextcloud_dump.sql.gz"
 
-# Nettoyage ancien backup
+# Nettoyage ancien backup +30 jours
 find /srv/backup/postgres/ -maxdepth 1 -type d -mtime +30 -exec rm -rf {} \;
